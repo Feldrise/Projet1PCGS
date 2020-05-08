1a3274a70d71deae141e94c7ffcf31871e04c39e
Feldrise <admin@feldrise.com>
2020-05-08 19:02:38 +0200

    [Add] License

--- projet1.py
+++ projet1.py
@@ -1,3 +1,26 @@
+##########################################################################
+# Copyright 2020 Victor (Feldrise) DENIS <contact@feldrise.com>          #
+#                                                                        #
+# Permission is hereby granted, free of charge, to any person            #
+# obtaining a copy of this software and associated documentation         #
+# files (the "Software"), to deal in the Software without restriction,   #
+# including without limitation the rights to use, copy, modify, merge,   #
+# publish, distribute, sublicense, and/or sell copies of the Software,   #
+# and to permit persons to whom the Software is furnished to do so,      #
+# subject to the following conditions:                                   #
+#                                                                        #
+# The above copyright notice and this permission notice shall be         #
+# included in all copies or substantial portions of the Software.        #
+#                                                                        #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     #
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. #
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   #
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   #
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      #
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 #
+##########################################################################
+
 import numpy as np
 import time
 import random
@@ -253,20 +276,22 @@ def jouerPartie():
 
         if coupGagnant(ligneJoue, colonneJoue, joueur):
             print()
-            print("==========")
             if joueur == 1:
-                print(" VAINQUEUR : vous")
+                print("#####################")
+                print("# Vous avez gagné ! #")
+                print("#####################")
             else:
-                print(" VAINQUEUR : l'ordinateur")
-            print("==========")
+                print("#####################")
+                print("# Vous avez perdu ! #")
+                print("#####################")
             print()
 
             partieFini = True
         elif jeuEncorePossible() == False:
             print()
-            print("==========")
-            print(" FIN DU JEU. Il n'y a pas de vainqueur...")
-            print("==========")
+            print("############################################")
+            print("# FIN DU JEU. Il n'y a pas de vainqueur... #")
+            print("############################################")
             print()
         else:
             joueurSuivant()
